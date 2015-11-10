fb0486c91f5e92ddedfb5bff958d28baf11e362f
Kapil Marwaha <kapilmar@gmail.com>
2015-11-10 18:28:30 +0530

    Update zip_compact.py

--- zip_compact.py
+++ zip_compact.py
@@ -1,24 +1,32 @@
 import zipfile
-import os
+# import os
 
-def unzip(from_path, to_path):
+# def unzip(from_path, to_path):
 
-    zip_ref = zipfile.ZipFile(from_path, 'r')
-    zip_ref.extractall(to_path)
-    zip_ref.close()
+    # zip_ref = zipfile.ZipFile(from_path, 'r')
+    # zip_ref.extractall(to_path)
+    # zip_ref.close()
 	
 for zip_file in input_eod_list:
 
-    zip_file = ("D:/Python - Stuff/IEOD Project/Input/EOD/%s" % zip_file)
+    zip_file = ("H:/IEOD/2015_07_July_02/%s" % zip_file)
     print zip_file
+    zin = zipfile.ZipFile (zip_file, 'r')
+	zout = zipfile.ZipFile ('archve_new.zip', 'w')
+	for item in zin.infolist():
+		buffer = zin.read(item.filename)
+		if (item.filename[-4:] != '.exe'):
+			zout.writestr(item, buffer)
+	zout.close()
+	zin.close()	
 
-    unzip(zip_file, zip_output_eod_path)	
+    # unzip(zip_file, zip_output_eod_path)	
 	
-zin = zipfile.ZipFile ('archive.zip', 'r')
-zout = zipfile.ZipFile ('archve_new.zip', 'w')
-for item in zin.infolist():
-    buffer = zin.read(item.filename)
-    if (item.filename[-4:] != '.exe'):
-        zout.writestr(item, buffer)
-zout.close()
-zin.close()	
+# zin = zipfile.ZipFile ('archive.zip', 'r')
+# zout = zipfile.ZipFile ('archve_new.zip', 'w')
+# for item in zin.infolist():
+    # buffer = zin.read(item.filename)
+    # if (item.filename[-4:] != '.exe'):
+        # zout.writestr(item, buffer)
+# zout.close()
+# zin.close()	
